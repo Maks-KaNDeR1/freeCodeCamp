6573359c219b12b8a003174a2559ac28da9d676f
Maks-KaNDeR <makskander1@gmail.com>
2021-11-21 12:17:05 +0300

    freeCodeCamp/JavaScript

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/40-44.js
@@ -0,0 +1,30 @@
+// 40 - 
+const myArray = [50, 60, 70];
+var myData = myArray[0]
+
+// 41 - 
+// Setup
+const myArray = [18, 64, 99];
+myArray[0] = 45
+// Only change code below this line
+
+// 42 - 
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[2][1];
+// 43 - 
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3])
+// Only change code below this line
+
+// 44 -
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop()
+// Only change code below this line
