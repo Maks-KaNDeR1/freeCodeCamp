1c014e88d2c294ef9ee98155cdae1d8a76435ff5
Maks-KaNDeR <makskander1@gmail.com>
2021-11-24 15:12:04 +0300

    react 8-10

--- Front End Development Librairies Certification/React/6-10.js
+++ Front End Development Librairies Certification/React/6-10.js
@@ -13,3 +13,78 @@ const MyComponent = function () {
         <div>Completed challenge!</div>
     );
 }
+
+// 8 
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hello React!</h1>
+            </div>
+        );
+    }
+};
+
+// 9
+const ChildComponent = () => {
+    return (
+        <div>
+            <p>I am the child</p>
+        </div>
+    );
+};
+
+class ParentComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>I am the parent</h1>
+                <ChildComponent />
+            </div>
+        );
+    }
+}
+
+// 10
+const TypesOfFruit = () => {
+    return (
+        <div>
+            <h2>Fruits:</h2>
+            <ul>
+                <li>Apples</li>
+                <li>Blueberries</li>
+                <li>Strawberries</li>
+                <li>Bananas</li>
+            </ul>
+        </div>
+    );
+};
+
+const Fruits = () => {
+    return (
+        <div>
+            <TypesOfFruit />
+        </div>
+    );
+};
+
+class TypesOfFood extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Types of Food:</h1>
+                <Fruits />
+            </div>
+        );
+    }
+};
