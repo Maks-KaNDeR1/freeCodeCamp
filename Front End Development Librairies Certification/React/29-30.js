18ebe93cf2e64cbd99daa3968bbcd4e8c5cdb1b1
Maks-KaNDeR <makskander1@gmail.com>
2021-12-01 20:34:37 +0300

    27-32

--- Front End Development Librairies Certification/React/29-30.js
+++ Front End Development Librairies Certification/React/29-30.js
@@ -1,4 +1,29 @@
-class MyForm extends React.Component {
+class ControlledInput extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: ''
+      };
+      this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange (event) {
+      this.setState({
+        input: event.target.value
+      })
+    }
+    render() {
+      return (
+        <div>
+          <input value={this.state.input} onChange={this.handleChange} />
+          <h4>Controlled Input:</h4>
+          <p>{this.state.input}</p>
+        </div>
+      );
+    }
+  };
+
+  // 30
+  class MyForm extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
