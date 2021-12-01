7b16efba5e653b25d3c2ee333c7a1a57687b340a
Maks-KaNDeR <makskander1@gmail.com>
2021-12-01 20:57:49 +0300

    react 34

---
+++ Front End Development Librairies Certification/React/33.js
@@ -0,0 +1,11 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    componentWillMount() {
+    console.log("I'm BOSS");
+    }
+    render() {
+      return <div />
+    }
+  };
